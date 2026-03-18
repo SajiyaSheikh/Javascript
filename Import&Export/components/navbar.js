561a0eb7030cf57e6d0821228901df9ea51cef7f
SajiyaSheikh <sajusheikh1745@gmail.com>
2026-03-18 12:52:08 +0530

    JS Practice

---
+++ Import&Export/components/navbar.js
@@ -0,0 +1,13 @@
+function navbar() {
+    return ` <nav id="navbar">
+        <div>
+            <a href="#">Home</a>
+        </div>
+        <div id="right-nav">
+            <a href="">User</a>
+            <a href="">Post</a>
+            <a href="">Login</a>
+        </div>
+    </nav> `
+}
+export default navbar
